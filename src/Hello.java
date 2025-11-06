1ce20207c53cf6f0418f28249e7b313fb42ba89c
Urvi Thakkar <urvithakkar21@gmail.com>
2025-11-06 16:28:56 -0600

    java basic

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+import javax.xml.transform.stream.StreamSource;
+import java.sql.SQLOutput;
+
+public class Hello {
+
+    public static void main(String args[]){
+
+        boolean as = false;
+       int a=2;
+       int b=7;
+       int c = a + b;
+        System.out.println("Addition : " + c);
+        System.out.println(as);
+
+    }
+}
