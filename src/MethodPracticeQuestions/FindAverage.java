99015cb064f02d7c8760d9cc756a3c718587ff07
Urvi Thakkar <urvithakkar21@gmail.com>
2026-01-12 13:27:40 -0600

    Java Method Practice Questions

---
+++ src/MethodPracticeQuestions/FindAverage.java
@@ -0,0 +1,21 @@
+package MethodPracticeQuestions;
+
+public class FindAverage {
+
+    public static void main(String[] args) {
+
+        double mathScore=45.4;
+        double scienceScore=55.5;
+        double englishScore=65.6;
+
+        FindAverage fa=new FindAverage();
+        double averageMarks=fa.FindAverageMarks(mathScore,scienceScore,englishScore);
+        System.out.println("The Average Marks are: "+averageMarks);
+    }
+
+    public double FindAverageMarks(double mathScore,double scienceScore,double englishScore){
+        double averageScore;
+        averageScore = (mathScore+scienceScore+englishScore)/3;
+        return averageScore;
+    }
+}
